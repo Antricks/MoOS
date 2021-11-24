8d18340b0761f5cda3657ad4351e62dc6004fc32
Antricks <33262801+Antricks@users.noreply.github.com>
2021-11-24 17:09:18 +0100

    add nice greeting to main screen

--- kernel.cpp
+++ kernel.cpp
@@ -45,6 +45,8 @@ extern "C" void kernelMain(const void* multiboot_structure, unsigned int multibo
     interrupts.activate();
     puts("done");
 
+    print("Currently this screen is the only thing I can offer here. Feel free to move around your mouse or type something nice... Have fun ;D");
+
     while(true) {
 
     }
